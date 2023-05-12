d45a22dc916345d54e36c729d1aa391cbaf08e8a
Lucas Silva <lucasiilva03@gmail.com>
2023-05-12 18:52:10 +0100

    db connection

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const config = {
   // if environment variables are not defined, use default values
   USER: process.env.DB_USER || 'lucasilva03',
   PASSWORD: process.env.DB_PASSWORD || 'Lucas.123',
-  DB: process.env.DB_NAME || 'tutorialsDB',
+  DB: process.env.DB_NAME || 'PhotoRecycle',
 };
 config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.nrqcqnv.mongodb.net/${config.DB}?retryWrites=true&w=majority
 `;
