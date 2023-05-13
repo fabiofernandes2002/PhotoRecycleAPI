126e5859a30d124b477dcb4dab1f3e06ae73eebb
Fábio Fernandes <75033072+fabiofernandes2002@users.noreply.github.com>
2023-05-14 00:16:25 +0100

    Atualização da conexão da BD

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,15 @@
 const config = {
   /* don't expose password or any sensitive info, done only for demo */
   // if environment variables are not defined, use default values
-  USER: process.env.DB_USER || 'lucasilva03',
+  USER: process.env.DB_USER || 'PhotoRecycle',
   PASSWORD: process.env.DB_PASSWORD || 'Lucas.123',
-  DB: process.env.DB_NAME || 'PhotoRecycle',
+  DB: process.env.DB_NAME || 'PhotoRecycleDB',
+  SECRET : process.env.SECRET
 };
-config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.nrqcqnv.mongodb.net/${config.DB}?retryWrites=true&w=majority
+config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.jow2ps4.mongodb.net/${config.DB}?retryWrites=true&w=majority
 `;
 module.exports = config;
+
+
+
+// mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.nrqcqnv.mongodb.net/${config.DB}?retryWrites=true&w=majority
