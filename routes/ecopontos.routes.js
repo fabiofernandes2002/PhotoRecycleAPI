92b365e1de92cfe206818fd3d74bd16b9f1d3e92
Lucas Silva <lucasiilva03@gmail.com>
2023-05-16 16:32:27 +0100

    updated routes to use new controller

--- routes/ecopontos.routes.js
+++ routes/ecopontos.routes.js
@@ -13,9 +13,18 @@ router.use((req, res, next) => {
 });
 
 router.route('/').get(ecopontosController.findAll);
+
 /* .post(ecopontosController.create); */
 
 router.route('/:ecopontoID').get(ecopontosController.findOne);
+
+router.route('/:ecopontoID/use').post(ecopontosController.useEcopoint);
+
+/* router.route('/:ecopontoID/validate').post(ecopontosController.validateEcopoint); */
+
+/* router.route('/:ecopontoID').put(ecopontosController.update); */
+
+router.route('/:ecopontoID').delete(ecopontosController.delete);
 
 router.all('*', function (req, res) {
   res.status(404).json({ message: 'What???' });
