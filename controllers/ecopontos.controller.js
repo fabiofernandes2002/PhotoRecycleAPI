877c18507e874e8be63ea8a2e248174df6ef3d1e
Bernardo2814 <40210311@esmad.ipp.pt>
2023-06-13 17:05:12 +0100

    organização

--- controllers/ecopontos.controller.js
+++ controllers/ecopontos.controller.js
@@ -12,6 +12,136 @@ cloudinary.config({
   api_secret: config.CLOUDINARY_API_SECRET,
 });
 
+exports.findAll = async (req, res) => {
+  const tipo = req.query.tipo;
+
+  const condition = tipo ? { tipo: new RegExp(tipo, 'i') } : {};
+  try {
+    // find function parameters: filter, projection (select) / returns a list of documents
+    let ecopontos = await Ecopoint.find(condition)
+      .select(
+        'nome criador localizacao morada dataCriacao foto tipo latitude longitude validacao'
+      ) // select the fields: do not show versionKey field
+      .exec(); // execute the query
+    res.status(200).json({ success: true, ecopontos: ecopontos });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde.',
+    });
+  }
+};
+
+exports.findOne = async (req, res) => {
+  try {
+    // findById(id) === findOne( {_id : id })
+    const ecopoint = await Ecopoint.findById(req.params.ecopointID).exec();
+
+    if (ecopoint === null)
+      return res.status(404).json({
+        success: false,
+        msg: `Não foi possível encontrar o ecoponto como o ID: ${req.params.ecopointID}.`,
+      });
+    res.status(200).json({
+      success: true,
+      ecoponto: ecopoint,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg: 'Algo deu errado. Por favor, tente novamente mais tarde.',
+    });
+  }
+};
+
+// validar ecoponto passando a validacao para true mas só se o utilizador estiver autenticado e for admin
+exports.validateEcopoint = async (req, res) => {
+  if (req.loggedUserType != 'admin') {
+    return res.status(403).json({
+        success: false,
+        msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
+    });
+  }
+
+  try {
+    const ecopoint = await Ecopoint.findByIdAndUpdate(req.params.ecopointID, { validacao: true }, { new: true, useFindAndModify: false });
+    if (!ecopoint) {
+        return res.status(404).json({
+            success: false,
+            msg: `Não foi possível encontrar o ecoponto com o ID: ${req.params.ecopointID}.`,
+        });
+    }
+    res.status(200).json({
+        success: true,
+        msg: `O ecoponto com o ID: ${req.params.ecopointID} foi validado com sucesso!`,
+    });
+
+    // atribuir pontos ao utilizador
+    const utilizador = await User.findById(ecopoint.criador).exec();
+    utilizador.pontos += 50;
+    // guardar o utilizador na base de dados
+    await utilizador.save();
+    res.status(200).json({
+      success: true,
+      msg: `Foram atribuídos 10 pontos ao utilizador com o ID: ${ecopoint.criador}.`,
+    });
+
+  } catch (err) {
+    res.status(500).json({
+        success: false,
+        msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde.',
+    });
+  }
+};
+
+// Function to use the Ecopoint model
+exports.useEcopoint = async (req, res) => {
+  try {
+    const ecopointID = req.params.id;
+
+    const ecopoint = await Ecopoint.findById(ecopointID).exec();
+
+    let image_utilizacao = null;
+    if (req.file) {
+      image_utilizacao = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'utilizacoes',
+        crop: 'scale',
+      })
+    }
+
+    // usar registo de utilização para guardar o idUtilizador, idEcoponto, dataUtilizacao, foto, validacao
+    const registoUtilizacao = new RegistoUtilizacao({
+      idUtilizador: req.loggedUserID,
+      idEcoponto: ecopointID,
+      dataUtilizacao: Date.now(),
+      foto: image_utilizacao.secure_url,
+      validacao: false,
+    });
+
+    // save the registoUtilizacao in the database
+    await registoUtilizacao.save();
+
+    if (!ecopoint) {
+      return res.status(404).json({
+        success: false,
+        msg: `Não foi possível encontrar o ecoponto com o ID: ${ecopointID}.`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      ecoponto: ecopoint,
+      /* falta adicionar a utilização do ecoponto pelo utilizador */
+      msg: `O ecoponto com o ID: ${ecopointID} foi utilizado com sucesso.`,
+    });
+ 
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg: 'Algo deu errado. Por favor, tente novamente mais tarde.',
+    });
+  }
+};
+
 // Registar um novo ecoponto
 exports.createAdicaoEcoponto = async (req, res) => {
   const validacao = User.tipo === "admin" ? true : false;
@@ -79,136 +209,6 @@ exports.createAdicaoEcoponto = async (req, res) => {
       });
   }
 }
-
-exports.findAll = async (req, res) => {
-  const tipo = req.query.tipo;
-
-  const condition = tipo ? { tipo: new RegExp(tipo, 'i') } : {};
-  try {
-    // find function parameters: filter, projection (select) / returns a list of documents
-    let ecopontos = await Ecopoint.find(condition)
-      .select(
-        'nome criador localizacao morada dataCriacao foto tipo latitude longitude validacao'
-      ) // select the fields: do not show versionKey field
-      .exec(); // execute the query
-    res.status(200).json({ success: true, ecopontos: ecopontos });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde.',
-    });
-  }
-};
-
-exports.findOne = async (req, res) => {
-  try {
-    // findById(id) === findOne( {_id : id })
-    const ecopoint = await Ecopoint.findById(req.params.ecopointID).exec();
-
-    if (ecopoint === null)
-      return res.status(404).json({
-        success: false,
-        msg: `Não foi possível encontrar o ecoponto como o ID: ${req.params.ecopointID}.`,
-      });
-    res.status(200).json({
-      success: true,
-      ecoponto: ecopoint,
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      msg: 'Algo deu errado. Por favor, tente novamente mais tarde.',
-    });
-  }
-};
-
-// Function to use the Ecopoint model
-exports.useEcopoint = async (req, res) => {
-  try {
-    const ecopointID = req.params.id;
-
-    const ecopoint = await Ecopoint.findById(ecopointID).exec();
-
-    let image_utilizacao = null;
-    if (req.file) {
-      image_utilizacao = await cloudinary.uploader.upload(req.file.path, {
-        folder: 'utilizacoes',
-        crop: 'scale',
-      })
-    }
-
-    // usar registo de utilização para guardar o idUtilizador, idEcoponto, dataUtilizacao, foto, validacao
-    const registoUtilizacao = new RegistoUtilizacao({
-      idUtilizador: req.loggedUserID,
-      idEcoponto: ecopointID,
-      dataUtilizacao: Date.now(),
-      foto: image_utilizacao.secure_url,
-      validacao: false,
-    });
-
-    // save the registoUtilizacao in the database
-    await registoUtilizacao.save();
-
-    if (!ecopoint) {
-      return res.status(404).json({
-        success: false,
-        msg: `Não foi possível encontrar o ecoponto com o ID: ${ecopointID}.`,
-      });
-    }
-    res.status(200).json({
-      success: true,
-      ecoponto: ecopoint,
-      /* falta adicionar a utilização do ecoponto pelo utilizador */
-      msg: `O ecoponto com o ID: ${ecopointID} foi utilizado com sucesso.`,
-    });
- 
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      msg: 'Algo deu errado. Por favor, tente novamente mais tarde.',
-    });
-  }
-};
-
-// validar ecoponto passando a validacao para true mas só se o utilizador estiver autenticado e for admin
-exports.validateEcopoint = async (req, res) => {
-  if (req.loggedUserType != 'admin') {
-    return res.status(403).json({
-        success: false,
-        msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
-    });
-  }
-
-  try {
-    const ecopoint = await Ecopoint.findByIdAndUpdate(req.params.ecopointID, { validacao: true }, { new: true, useFindAndModify: false });
-    if (!ecopoint) {
-        return res.status(404).json({
-            success: false,
-            msg: `Não foi possível encontrar o ecoponto com o ID: ${req.params.ecopointID}.`,
-        });
-    }
-    res.status(200).json({
-        success: true,
-        msg: `O ecoponto com o ID: ${req.params.ecopointID} foi validado com sucesso!`,
-    });
-
-    // atribuir pontos ao utilizador
-    const utilizador = await User.findById(ecopoint.criador).exec();
-    utilizador.pontos += 50;
-    // guardar o utilizador na base de dados
-    await utilizador.save();
-    res.status(200).json({
-      success: true,
-      msg: `Foram atribuídos 10 pontos ao utilizador com o ID: ${ecopoint.criador}.`,
-    });
-
-  } catch (err) {
-    res.status(500).json({
-        success: false,
-        msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde.',
-    });
-  }
-};
 
 exports.deleteEcopontoById = async (req, res) => {
   try {
--- controllers/utilizacoes.controller.js
+++ controllers/utilizacoes.controller.js
@@ -6,13 +6,47 @@ const config = require("../config/db.config.js");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: config.CLOUDINARY_CLOUD_NAME,
-  api_key: config.CLOUDINARY_API_KEY,
-  api_secret: config.CLOUDINARY_API_SECRET,
+    cloud_name: config.CLOUDINARY_CLOUD_NAME,
+    api_key: config.CLOUDINARY_API_KEY,
+    api_secret: config.CLOUDINARY_API_SECRET,
 });
 
+// Retrieve all RegistoUtilizacaos from the database.
+exports.findAllRegistoUtilizacoes = async (req, res) => {
+    const idUtilizador = req.query.idUtilizador;
+    const condition = idUtilizador
+        ? {
+            idUtilizador: {
+                $regex: new RegExp(idUtilizador),
+                $options: 'i',
+            },
+        }
+        : {};
+
+    try {
+        if (req.loggedUserType != 'admin') {
+            return res.status(403).json({
+                success: false,
+                msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
+            });
+        }
+        const registoUtilizacao = await RegistoUtilizacao.find(condition).
+            select('idUtilizador idEcoponto dataUtilizacao foto validacao').
+            exec();
+        res.status(200).json({
+            success: true,
+            registoUtilizacoes: registoUtilizacao,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
+        });
+    }
+}
+
 // create utilizaco
-exports.createUtilizacao = async (req,res) => {
+exports.createUtilizacao = async (req, res) => {
 
     try {
         const ecopointID = req.params.id;
@@ -21,15 +55,15 @@ exports.createUtilizacao = async (req,res) => {
 
         let image_utilizacao = null;
         if (req.file) {
-        image_utilizacao = await cloudinary.uploader.upload(req.file.path, {
-            folder: "Utilizacões",
-            crop: "scale",
-        });
-        } else{
-        return res.status(400).json({
-            success: false,
-            msg: "Coloque uma foto.",
-        });
+            image_utilizacao = await cloudinary.uploader.upload(req.file.path, {
+                folder: "Utilizacões",
+                crop: "scale",
+            });
+        } else {
+            return res.status(400).json({
+                success: false,
+                msg: "Coloque uma foto.",
+            });
         }
 
         const registoUtilizacao = new RegistoUtilizacao({
@@ -39,14 +73,14 @@ exports.createUtilizacao = async (req,res) => {
             foto: image_utilizacao.secure_url,
             validacao: false,
         });
-    
+
         // save the registoUtilizacao in the database
         await registoUtilizacao.save();
 
         if (!utilizacao) {
             return res.status(404).json({
-            success: false,
-            msg: `Não foi possível encontrar o ecoponto com o ID: ${ecopointID}.`,
+                success: false,
+                msg: `Não foi possível encontrar o ecoponto com o ID: ${ecopointID}.`,
             });
         }
         res.status(200).json({
@@ -65,18 +99,8 @@ exports.createUtilizacao = async (req,res) => {
 
 }
 
-// Retrieve all RegistoUtilizacaos from the database.
-exports.findAllRegistoUtilizacoes = async (req, res) => {
-  const idUtilizador = req.query.idUtilizador;
-  const condition = idUtilizador
-    ? {
-        idUtilizador: {
-          $regex: new RegExp(idUtilizador),
-          $options: 'i',
-        },
-      }
-    : {};
-
+// list all registoUtilizacoes from the database is validation == true.
+exports.findAllRegistoUtilizacoesValidados = async (req, res) => {
     try {
         if (req.loggedUserType != 'admin') {
             return res.status(403).json({
@@ -84,12 +108,42 @@ exports.findAllRegistoUtilizacoes = async (req, res) => {
                 msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
             });
         }
-        const registoUtilizacao = await RegistoUtilizacao.find(condition).
-        select('idUtilizador idEcoponto dataUtilizacao foto validacao').
-        exec();
+
+        const data = await RegistoUtilizacao.find({
+            validacao: true
+        }).
+            select('idUtilizador idEcoponto dataUtilizacao foto validacao').
+            exec();
         res.status(200).json({
             success: true,
-            registoUtilizacoes: registoUtilizacao,
+            registoUtilizacoes: data
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
+        });
+    }
+}
+
+// list all registoUtilizacoes from the database is validation == false.
+exports.findAllRegistoUtilizacoesNaoValidados = async (req, res) => {
+    try {
+        if (req.loggedUserType != 'admin') {
+            return res.status(403).json({
+                success: false,
+                msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
+            });
+        }
+
+        const data = await RegistoUtilizacao.find({
+            validacao: false
+        }).
+            select('idUtilizador idEcoponto dataUtilizacao foto validacao').
+            exec();
+        res.status(200).json({
+            success: true,
+            registoUtilizacoes: data
         });
     } catch (err) {
         res.status(500).json({
@@ -101,7 +155,7 @@ exports.findAllRegistoUtilizacoes = async (req, res) => {
 
 // Find a single RegistoUtilizacao with an id
 exports.findOneRegistoUtilizacao = async (req, res) => {
-  const idRegistoUtilizacao = req.params.idRegistoUtilizacao;
+    const idRegistoUtilizacao = req.params.idRegistoUtilizacao;
 
     try {
         if (req.loggedUserType != 'admin') {
@@ -111,8 +165,8 @@ exports.findOneRegistoUtilizacao = async (req, res) => {
             });
         }
         const data = await RegistoUtilizacao.findById(idRegistoUtilizacao).
-        select('idUtilizador idEcoponto dataUtilizacao foto validacao').
-        exec();
+            select('idUtilizador idEcoponto dataUtilizacao foto validacao').
+            exec();
         if (!data)
             return res.status(404).json({
                 success: false,
@@ -128,11 +182,11 @@ exports.findOneRegistoUtilizacao = async (req, res) => {
             msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
         });
     }
-}
+};
 
 // Delete a RegistoUtilizacao with the specified id in the request
 exports.deleteRegistoUtilizacao = async (req, res) => {
-  const idRegistoUtilizacao = req.params.idRegistoUtilizacao;
+    const idRegistoUtilizacao = req.params.idRegistoUtilizacao;
 
     try {
         if (req.loggedUserType != 'admin') {
@@ -159,113 +213,59 @@ exports.deleteRegistoUtilizacao = async (req, res) => {
             msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
         });
     }
-}
-
-// list all registoUtilizacoes from the database is validation == true.
-exports.findAllRegistoUtilizacoesValidados = async (req, res) => {
-    try {
-        if (req.loggedUserType != 'admin') {
-            return res.status(403).json({
-                success: false,
-                msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
-            });
-        }
-
-        const data = await RegistoUtilizacao.find({
-            validacao: true
-        }).
-        select('idUtilizador idEcoponto dataUtilizacao foto validacao').
-        exec();
-        res.status(200).json({
-            success: true,
-            registoUtilizacoes: data
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
-        });
-    }
-}
-
-// list all registoUtilizacoes from the database is validation == false.
-exports.findAllRegistoUtilizacoesNaoValidados = async (req, res) => {
-    try {
-        if (req.loggedUserType != 'admin') {
-            return res.status(403).json({
-                success: false,
-                msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
-            });
-        }
-
-        const data = await RegistoUtilizacao.find({
-            validacao: false
-        }).
-        select('idUtilizador idEcoponto dataUtilizacao foto validacao').
-        exec();
-        res.status(200).json({
-            success: true,
-            registoUtilizacoes: data
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
-        });
-    }
-}
+};
 
 // validadr a utilização de um ecoponto por admin quando estiver logado passando a validação para true
 exports.validarRegistoUtilizacao = async (req, res) => {
-  const idRegistoUtilizacao = req.params.idRegistoUtilizacao;
-  const registoUtilizacao = req.body;
+    const idRegistoUtilizacao = req.params.idRegistoUtilizacao;
+    const registoUtilizacao = req.body;
 
-  try {
-    if (req.loggedUserType != 'admin') {
-        return res.status(403).json({
-            success: false,
-            msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
+    try {
+        if (req.loggedUserType != 'admin') {
+            return res.status(403).json({
+                success: false,
+                msg: 'Apenas o administrador pode aceder a esta funcionalidade!',
+            });
+        }
+        const data = await RegistoUtilizacao.findByIdAndUpdate(idRegistoUtilizacao, registoUtilizacao, {
+            new: true,
+            runValidators: true,
         });
-    }
-    const data = await RegistoUtilizacao.findByIdAndUpdate(idRegistoUtilizacao, registoUtilizacao, {
-      new: true,
-      runValidators: true,
-    });
-    if (!data)
-      return res.status(404).json({
-        success: false,
-        msg: 'Registo de utilização não encontrado!',
-      });
-    else
-      res.status(200).json({
-        success: true,
-        msg: 'Utilização do ecoponto validada co sucesso!',
-        URL: `/registoUtilizacoes/${idRegistoUtilizacao}`,
-      });
+        if (!data)
+            return res.status(404).json({
+                success: false,
+                msg: 'Registo de utilização não encontrado!',
+            });
+        else
+            res.status(200).json({
+                success: true,
+                msg: 'Utilização do ecoponto validada co sucesso!',
+                URL: `/registoUtilizacoes/${idRegistoUtilizacao}`,
+            });
 
-      // atribuir pontos ao utilizador que fez a utilização do ecoponto
+        // atribuir pontos ao utilizador que fez a utilização do ecoponto
         const utilizador = await User.findById(data.idUtilizador);
         const pontos = utilizador.pontos + 10;
         utilizador.pontos = pontos;
-        await User.findByIdAndUpdate(data.idUtilizador, utilizador, { 
+        await User.findByIdAndUpdate(data.idUtilizador, utilizador, {
             new: true,
             runValidators: true,
         });
 
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      let errors = [];
-      Object.keys(err.errors).forEach((key) => {
-        errors.push(err.errors[key].message);
-      });
-      return res.status(400).json({
-        success: false,
-        msgs: errors,
-      });
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            let errors = [];
+            Object.keys(err.errors).forEach((key) => {
+                errors.push(err.errors[key].message);
+            });
+            return res.status(400).json({
+                success: false,
+                msgs: errors,
+            });
+        }
+        res.status(500).json({
+            success: false,
+            msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
+        });
     }
-    res.status(500).json({
-      success: false,
-      msg: err.message || 'Algo deu errado. Por favor, tente novamente mais tarde. ',
-    });
-  }
 };
